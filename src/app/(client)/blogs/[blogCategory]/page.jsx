45c5ec8acbc7e53cb710d6731e19c687f4e9cf3b
asifaowadud <asifaowadud@gmail.com>
2024-10-19 12:40:55 +0600

    rady for deploy on vercel withou backend

--- src/app/(client)/blogs/[blogCategory]/page.jsx
+++ src/app/(client)/blogs/[blogCategory]/page.jsx
@@ -1,48 +1,22 @@
-// import AllBlogs from "@/components/blogsPage/AllBlogs";
-
-// export async function generateStaticParams() {
-//   const { categories } = await fetch(
-//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/blogs/categories/unique`
-//   ).then((res) => res.json());
-
-//   const staticParams4CategorisedBlogs = categories.map((category) => ({
-//     blogCategory: category,
-//   }));
-//   return staticParams4CategorisedBlogs;
-// }
-
-// export const metadata = {
-//   title: {
-//     absolute: "Blog - JMC Tours & Travels", // it overwrites default title of layout.js
-//   },
-//   description: "Blog",
-// };
-
-// const CategorisedBlogs = ({ params }) => {
-//   return <AllBlogs blogCategory={params?.blogCategory} />;
-// };
-
-// export default CategorisedBlogs;
-
 import AllBlogs from "@/components/blogsPage/AllBlogs";
 
-// export async function generateStaticParams() {
-//   const { categories } = await fetch(
-//     `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/blogs/categories/unique`
-//   ).then((res) => res.json());
+export async function generateStaticParams() {
+  const { categories } = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/blogs/categories/unique`
+  ).then((res) => res.json());
 
-//   const staticParams4CategorisedBlogs = categories.map((category) => ({
-//     blogCategory: category,
-//   }));
-//   return staticParams4CategorisedBlogs;
-// }
+  const staticParams4CategorisedBlogs = categories.map((category) => ({
+    blogCategory: category,
+  }));
+  return staticParams4CategorisedBlogs;
+}
 
-// export const metadata = {
-//   title: {
-//     absolute: "Blog - JMC Tours & Travels", // it overwrites default title of layout.js
-//   },
-//   description: "Blog",
-// };
+export const metadata = {
+  title: {
+    absolute: "Blog - JMC Tours & Travels", // it overwrites default title of layout.js
+  },
+  description: "Blog",
+};
 
 const CategorisedBlogs = ({ params }) => {
   return <AllBlogs blogCategory={params?.blogCategory} />;
